8935fa088279f086320fe54d15fbba4ec4d4e74c
Arth Gupta <guptaarth87@gmail.com>
2021-10-13 13:29:31 +0530

    STcrudapi

---
+++ crud api mongo node/Controllers/Students.js
@@ -0,0 +1,112 @@
+const Students = require('../model/Students');
+
+exports.getAllStudents = (req, res) => {
+    Students.find().then(result => {       //find methond to fetch all data
+        console.log(result)
+        res.status(200).json({
+            message: "Students data fetched",
+            students: result
+        });
+    }).catch(error => {
+        res.status(500).json(
+            {
+            message: "Error in Database",
+            error: error
+           }
+        );
+    });
+}
+
+exports.getStudent=(req,res)=>{
+     const studentName=req.params.name;
+     Students.find({
+         name:studentName
+     }).then(result=>{
+         res.status(200).json({
+             message:"request successfull",
+             student:result
+         });
+     }).catch(err=>{
+         res.status(500).json({
+             message:"data not available error!",
+             error:err
+         })
+     })
+}
+
+exports.addNewStudent = (req, res) => {
+    //fetching details
+    const{                      
+        student_id,
+        name,
+        class_,
+        section,
+        subjects,          
+        address
+    }=req.body;
+
+    //create new object with user data
+   
+    const studentObj =new Students({
+    student_id: student_id,
+    name: name,
+    class_: class_,
+    subjects: subjects,
+    section:section,
+    subjects:subjects,
+    address:address
+  });
+  
+  //we will call method to save this in db
+  studentObj.save().then(
+      result=> {
+          res.status(200).json({
+              message:"student registered successfully !!",
+              Student:result
+          });
+      }).catch(error =>{
+          res.status(500).json({
+              message: "Error in Database",
+              error:error
+          });
+      });
+}
+
+exports.deleteStudent = (req, res) => {
+    const studentName = req.params.name;    //storing name paramerter in variable
+
+    Students.find({
+       name: studentName
+    }).then(result=>{
+        id=result[0].id
+        Students.findByIdAndRemove(id)
+        .then(data=>{
+            if (!data){
+                res.status(404).send({
+                    message :`cannot delete ${studentName} mat be doesnt exixt in db!`
+                });
+            }else{
+                res.send({
+                    message:"deleted succesfully !"
+                });
+            }
+        })
+    })
+}
+
+exports.updateStudent = (req, res) => {
+    const studentName = req.params.name;
+    Students.find({
+        name: studentName
+    }).then(result=>{
+      const  id=result[0].id
+       Students.findByIdAndUpdate({ _id: id }, req.body)
+       .then((data) => {
+         res.send("updated Successfully");
+       })
+       .catch((err) => {
+         res.send("err in updation");
+       });
+    })
+}
+
---
+++ crud api mongo node/Controllers/Teachers.js
@@ -0,0 +1,109 @@
+// const Teachers = require('../model/Teachers');
+const Teacher = require('../model/Teachers');
+
+exports.getAllTeachers = (req, res) => {
+    Teacher.find().then(result => {
+        // console.log(result)
+        res.status(200).json({
+            message: "Teachers data fetched",
+            teachers: result
+        });
+    }).catch(error => {
+        res.status(500).json(
+            {
+            message: "Error in Database",
+            error: error
+           }
+        );
+    });
+}
+
+exports.getTeacher=(req,res)=>{
+    const teacherName = req.params.name;
+    Teacher.find({
+        name: teacherName
+    }).then(result=>{
+        res.status(200).json({
+            message:"request successfull",
+            teacher:result
+        })
+    }).catch(err=>{
+        res.status(500).json({
+            message:"data not available error!",
+            error:err
+        });
+    })
+}
+
+exports.addNewTeacher = (req, res) => {
+    const{
+        teacher_id,
+        name,
+        subject,
+        classes,
+        salary
+    }=req.body;
+
+    //create new object with user data
+   
+    const teacherObj =new Teacher({
+    teacher_id: teacher_id,
+    name: name,
+    subject: subject,
+    classes: classes,
+    salary :salary
+  });
+  
+  //we will call method to save this in db
+  teacherObj.save().then(
+      result=> {
+          res.status(200).json({
+              message:"teacher registered successfully !!",
+              teacher:result
+          });
+      }).catch(error =>{
+          res.status(500).json({
+              message: "Error in Database",
+              error:error
+          });
+      });
+}
+
+exports.deleteTeacher = (req, res) => {
+        const teacherName = req.params.name;
+        Teacher.find({
+           name: teacherName
+        }).then(result=>{
+            id=result[0].id
+            Teacher.findByIdAndRemove(id)
+            .then(data=>{
+                if (!data){
+                    res.status(404).send({
+                        message :`cannot delete ${teacherName} mat be doesnt exixt in db!`
+                    });
+                }else{
+                    res.send({
+                        message:"deleted succesfully !"
+                    });
+                }
+            })
+        })
+}
+
+exports.updateTeacher = (req, res) => {
+    const teacherName = req.params.name;
+ Teacher.find({
+     name: teacherName
+ }).then(result=>{
+   const  id=result[0].id
+    Teacher.findByIdAndUpdate({ _id: id }, req.body)
+    .then((data) => {
+      res.send("updated Successfully");
+    })
+    .catch((err) => {
+      res.send("err in updation");
+    });
+ })
+ }
+
+
